ecf3af1bfd5710542b71c8749501c549a76de1e0
dileeppm123321 <dileepyadavpm@gmail.com>
2025-12-07 17:25:33 +0000

    Fix Cloud Run port binding

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
-  res.send("Backend running on VM");
+  res.send("Cloud Run is working");
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
